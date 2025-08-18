98826d2b930863f7c047fd6a3db4d0a758dee736
Shahed Nasser <shahednasser@gmail.com>
2025-08-18 14:09:55 +0300

    docs: create a new UI docs project

---
+++ www/apps/ui/generated/sidebar.mjs
@@ -0,0 +1,16 @@
+export const generatedSidebars = [
+  {
+    "sidebar_id": "ui",
+    "title": "Medusa UI",
+    "items": [
+      {
+        "loaded": true,
+        "isPathHref": true,
+        "type": "link",
+        "title": "Introduction",
+        "path": "/",
+        "children": []
+      }
+    ]
+  }
+]
