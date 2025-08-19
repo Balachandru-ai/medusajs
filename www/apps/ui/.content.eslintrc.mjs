815fb55efdd4eefc40317f8559344dcdb10e5676
Shahed Nasser <shahednasser@gmail.com>
2025-08-19 18:56:41 +0300

    lint fix

--- www/apps/ui/.content.eslintrc.mjs
+++ www/apps/ui/.content.eslintrc.mjs
@@ -150,6 +150,7 @@ export default [
       "@typescript-eslint/ban-ts-comment": "off",
       "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
       "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-require-imports": "off",
     },
   },
 ]
