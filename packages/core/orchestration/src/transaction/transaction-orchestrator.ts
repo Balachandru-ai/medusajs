253c952df1f47be8045f6f9b57b57330ae04bce6
adrien2p <adrien.deperetti@gmail.com>
2025-10-01 18:03:45 +0200

    fixes

--- packages/core/orchestration/src/transaction/transaction-orchestrator.ts
+++ packages/core/orchestration/src/transaction/transaction-orchestrator.ts
@@ -951,7 +951,7 @@ export class TransactionOrchestrator extends EventEmitter {
             this.executeSyncStep(promise, transaction, step, nextSteps)
           )
         } else {
-          // Execute async step in background and continue the execution of the transaction
+          // Execute async step in background as part of the next event loop cycle and continue the execution of the transaction
           process.nextTick(() =>
             this.executeAsyncStep(promise, transaction, step, nextSteps)
           )
