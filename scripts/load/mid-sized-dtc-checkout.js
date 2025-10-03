763cae0d759c0d5b500b96846ded86f80f6adf40
adrien2p <adrien.deperetti@gmail.com>
2025-10-03 12:41:35 +0200

    WIP

--- scripts/load/mid-sized-dtc-checkout.js
+++ scripts/load/mid-sized-dtc-checkout.js
@@ -22,25 +22,24 @@ import http from "k6/http"
 // let endpoint = __ENV.K6_ENDPOINT
 // let projectID = __ENV.K6_PROJECT_ID
 
-let publishableKey =
-  "pk_03823fccbc94952c4e2a6d045adb1b3479389ccacef6e4d5198c5e7b2a9dc4b5"
-let regionId = "reg_01K4JKNGBA9HXBS57EJFSXY2X7"
-let endpoint = "http://localhost:9000"
+// let publishableKey =
+//   "pk_03823fccbc94952c4e2a6d045adb1b3479389ccacef6e4d5198c5e7b2a9dc4b5"
+// let regionId = "reg_01K4JKNGBA9HXBS57EJFSXY2X7"
+// let endpoint = "http://localhost:9000"
 
 const firstStageDuration = "1m"
 const secondStageDuration = "3m"
 const thirdStageDuration = "1m"
 
-const highVus = 200
-// const mediumVus = 50
-const lowVus = 20
+const highVus = 300
+const lowVus = 30
 
-// let publishableKey =
-//   "pk_937f7a595bd4b039bb6bbb95476dd036dd79187f31ef61cf7093f2b81a1f863b"
-// let regionId = "reg_01K2ZDG12VKJ64F2NFTNW7Y8AT"
-// let endpoint = "https://dtc-starter-preview.medusajs.app"
+let publishableKey =
+  "pk_937f7a595bd4b039bb6bbb95476dd036dd79187f31ef61cf7093f2b81a1f863b"
+let regionId = "reg_01K2ZDG12VKJ64F2NFTNW7Y8AT"
+let endpoint = "https://dtc-starter-preview.medusajs.app"
 // let endpoint = "https://dtc-starter.medusajs.app"
-// let projectId = 4475269
+let projectId = 4837050
 
 const params = {
   headers: {
@@ -50,10 +49,10 @@ const params = {
 }
 
 export const options = {
-  //   cloud: {
-  //     projectID: projectId,
-  //     name: `Version 2.10.2, ${new Date().toLocaleString()} (local execution)`,
-  // },
+  cloud: {
+    projectID: projectId,
+    name: `Version 2.10.4-snapshot-20251003092840, ${new Date().toLocaleString()} (local execution - without caching)`,
+  },
   scenarios: {
     browseCatalog: {
       executor: "ramping-vus",
