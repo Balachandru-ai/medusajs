5ab62976c5dd2ab95c87a696f6e120495033d59a
Shahed Nasser <shahednasser@gmail.com>
2025-10-16 14:03:05 +0300

    improvements and fixes

--- www/apps/resources/app/references/[...slug]/page.tsx
+++ www/apps/resources/app/references/[...slug]/page.tsx
@@ -2,17 +2,7 @@ import { cache, Suspense } from "react"
 import { Loading } from "docs-ui"
 import path from "path"
 import fs from "fs/promises"
-import mdxOptions from "@/mdx-options.mjs"
-import {
-  typeListLinkFixerPlugin,
-  localLinksRehypePlugin,
-  workflowDiagramLinkFixerPlugin,
-  prerequisitesLinkFixerPlugin,
-  recmaInjectMdxDataPlugin,
-} from "remark-rehype-plugins"
-import { MDXRemote } from "next-mdx-remote-client/rsc"
-import MDXComponents from "../../../components/MDXComponents"
-import { notFound } from "next/navigation"
+import { ReferenceMDX } from "../../../components/ReferenceMDX"
 import { Metadata } from "next"
 import { getFrontMatterFromString } from "docs-utils"
 
@@ -26,92 +16,39 @@ export default async function ReferencesPage(props: PageProps) {
   const params = await props.params
   const { slug } = params
 
-  const fileData = await loadReferencesFile(slug)
-  if (!fileData) {
-    return notFound()
-  }
-
-  const pluginOptions = {
-    filePath: fileData.path,
-    basePath: process.cwd(),
-  }
-
   return (
     <Suspense fallback={<Loading />}>
       <div className="animate animate-fadeIn">
-        <MDXRemote
-          source={fileData.content}
-          components={MDXComponents}
-          options={{
-            disableImports: true,
-            mdxOptions: {
-              development: process.env.NEXT_PUBLIC_ENV === "development",
-              format: "mdx",
-              rehypePlugins: [
-                ...mdxOptions.options.rehypePlugins,
-                [
-                  typeListLinkFixerPlugin,
-                  {
-                    ...pluginOptions,
-                    checkLinksType: "md",
-                  },
-                ],
-                [
-                  workflowDiagramLinkFixerPlugin,
-                  {
-                    ...pluginOptions,
-                    checkLinksType: "value",
-                  },
-                ],
-                [
-                  prerequisitesLinkFixerPlugin,
-                  {
-                    ...pluginOptions,
-                    checkLinksType: "value",
-                  },
-                ],
-                [localLinksRehypePlugin, pluginOptions],
-              ],
-              remarkPlugins: [...mdxOptions.options.remarkPlugins],
-              recmaPlugins: [
-                [
-                  recmaInjectMdxDataPlugin,
-                  { isRemoteMdx: true, mode: process.env.NODE_ENV },
-                ],
-              ],
-            },
-          }}
-        />
+        <ReferenceMDX slug={slug} />
       </div>
     </Suspense>
   )
 }
 
-// export async function generateMetadata({
-//   params,
-// }: PageProps): Promise<Metadata> {
-//   // read route params
-//   const slug = (await params).slug
-//   const metadata: Metadata = {}
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const slug = (await params).slug
+  const metadata: Metadata = {}
 
-//   const fileData = await loadReferencesFile(slug)
+  const fileData = await loadReferencesFile(slug)
 
-//   if (!fileData) {
-//     return metadata
-//   }
+  if (!fileData) {
+    return metadata
+  }
 
-//   const pageTitleMatch = /#(?<title>[\w -]+)/.exec(fileData.content)
+  const pageTitleMatch = /#(?<title>[\w -]+)/.exec(fileData.content)
 
-//   if (!pageTitleMatch?.groups?.title) {
-//     return metadata
-//   }
+  if (!pageTitleMatch?.groups?.title) {
+    return metadata
+  }
 
-//   metadata.title = pageTitleMatch.groups.title
-//   const frontmatter = await getFrontMatterFromString(fileData.content)
-//   metadata.keywords = (frontmatter.keywords || []) as string[]
+  metadata.title = pageTitleMatch.groups.title
+  const frontmatter = await getFrontMatterFromString(fileData.content)
+  metadata.keywords = (frontmatter.keywords || []) as string[]
 
-//   return metadata
-// }
+  return metadata
+}
 
 export type LoadedReferenceFile = {
   content: string
@@ -136,6 +73,7 @@ const loadReferencesFile = cache(
     const fullPath = path.join(monoRepoPath, fileDetails.filePath)
 
     const fileContent = await fs.readFile(fullPath, "utf-8")
+
     return {
       content: fileContent,
       path: fullPath,
