6eaa95469690a9159ed17a5ecc2ea25076661f81
SteelRazor47 <steelrazor47@gmail.com>
2025-10-22 20:05:31 +0200

    fix generated deepMerge

--- packages/admin/admin-vite-plugin/src/i18n/generate-i18n.ts
+++ packages/admin/admin-vite-plugin/src/i18n/generate-i18n.ts
@@ -10,9 +10,16 @@ export async function generateI18n(sources: Set<string>) {
         return `import i18nTranslations${index} from "${normalizedPath}"`
     })
 
-    const mergeCode = indexFiles.length > 0
-        ? `deepMerge(${indexFiles.map((_, index) => `i18nTranslations${index}`).join(', ')})`
-        : '{}'
+    let mergeCode = '{}'
+    if (indexFiles.length === 1) {
+        mergeCode = 'i18nTranslations0'
+    } else if (indexFiles.length > 1) {
+        // Only happens in dev mode if there are 2+ plugins linked with plugin:develop
+        // Chain deepMerge calls since it only accepts 2 arguments
+        mergeCode = indexFiles.slice(1).reduce((acc, _, index) => {
+            return `deepMerge(${acc}, i18nTranslations${index + 1})`
+        }, 'i18nTranslations0')
+    }
 
     const code = outdent`
         resources: ${mergeCode}
