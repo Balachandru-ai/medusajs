5a1e2ca12f585ae6030c1099dd552cd8b198e12c
fPolic <mainacc.polic@gmail.com>
2025-10-22 18:31:38 +0200

    feat: cascade delete to pivot table, rm unused unused fields

--- packages/modules/product/src/models/product-variant-product-image.ts
+++ packages/modules/product/src/models/product-variant-product-image.ts
@@ -4,8 +4,6 @@ import ProductImage from "./product-image"
 
 const ProductVariantProductImage = model.define("ProductVariantProductImage", {
   id: model.id({ prefix: "pvpi" }).primaryKey(),
-  created_by: model.text().nullable(),
-  metadata: model.json().nullable(),
   variant: model.belongsTo(() => ProductVariant, {
     mappedBy: "images",
   }),
