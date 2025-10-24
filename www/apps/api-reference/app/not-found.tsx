e171cd4480d2d7f0b524308376cdee59afa92d8d
Shahed Nasser <shahednasser@gmail.com>
2025-10-24 08:50:56 +0300

    fix 404 page in api reference

--- www/apps/api-reference/app/not-found.tsx
+++ www/apps/api-reference/app/not-found.tsx
@@ -1,5 +1,14 @@
-import NotFoundContent from "./_not-found.mdx"
+import { AcademicCapSolid, BookOpen } from "@medusajs/icons"
 import clsx from "clsx"
+import { CardList, H1, H2, MDXComponents, SimilarPages } from "docs-ui"
+import { config } from "../config"
+
+const P = MDXComponents.p as React.FC<
+  React.HTMLAttributes<HTMLParagraphElement>
+>
+const A = MDXComponents.a as React.FC<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>
+>
 
 const NotFoundPage = () => {
   return (
@@ -12,7 +21,36 @@ const NotFoundPage = () => {
         "px-docs_1 md:px-docs_4"
       )}
     >
-      <NotFoundContent />
+      <H1>Page Not Found</H1>
+      <P>The page you were looking for isn&apos;t available.</P>
+      <P>
+        If you&apos;re looking for Medusa v1 documentation, it&apos;s been moved
+        to <A href="https://docs.medusajs.com/v1">docs.medusajs.com/v1</A>.
+      </P>
+      <P>
+        If you think this is a mistake, please{" "}
+        <A href="https://github.com/medusajs/medusa/issues/new?assignees=&labels=type%3A+docs&template=docs.yml">
+          report this issue on GitHub
+        </A>
+        .
+      </P>
+      <SimilarPages />
+      <H2>Other Resources</H2>
+      <CardList
+        itemsPerRow={2}
+        items={[
+          {
+            title: "Get Started Docs",
+            href: `${config.baseUrl}/learn`,
+            icon: BookOpen,
+          },
+          {
+            title: "Commerce Modules",
+            href: `${config.baseUrl}/resources/commerce-modules`,
+            icon: AcademicCapSolid,
+          },
+        ]}
+      />
     </div>
   )
 }
