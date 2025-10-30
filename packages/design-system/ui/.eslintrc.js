3b16fd831ae1fc5a821ded7829e75b5298754f25
adrien2p <adrien.deperetti@gmail.com>
2025-10-30 16:11:07 +0100

    chore(): Update dependencies usage

--- packages/design-system/ui/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: false, // Extend from root config
-  extends: [
-    "../../../.eslintrc.js", // Inherit root config
-    "plugin:storybook/recommended", // Add storybook rules only for this package
-  ],
-}
