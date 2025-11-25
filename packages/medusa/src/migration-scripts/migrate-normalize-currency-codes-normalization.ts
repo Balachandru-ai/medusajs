37620106a22ca17d0f76f8fa9531aade27c5b0ce
Nicolas Gorga <nicogorga11@gmail.com>
2025-11-25 07:24:55 -0300

    Remove unnecessary workflow, execute knex directly inside script

--- packages/medusa/src/migration-scripts/migrate-normalize-currency-codes-normalization.ts
+++ packages/medusa/src/migration-scripts/migrate-normalize-currency-codes-normalization.ts
@@ -1,30 +1,14 @@
-import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
-import { createStep, createWorkflow, WorkflowResponse } from "@medusajs/framework/workflows-sdk";
-import { ExecArgs } from "@medusajs/types";
+import { ContainerRegistrationKeys } from "@medusajs/framework/utils"
+import { ExecArgs } from "@medusajs/types"
 
-const updatePricesStep = createStep(
-    "update-prices",
-    async (_, { container }) => {
-        const knex = container.resolve(ContainerRegistrationKeys.PG_CONNECTION)
+export default async function migrateNormalizeCurrencyCodes({
+  container,
+}: ExecArgs) {
+  const knex = container.resolve(ContainerRegistrationKeys.PG_CONNECTION)
 
-        await knex.transaction(async (trx) => {
-          return await trx("price")
-            .update({
-              currency_code: knex.raw("LOWER(currency_code)"),
-            })
-        })
-    }
-)
-
-const normalizeCurrencyCodesWorkflow = createWorkflow(
-    'normalize-currency-codes',
-    () => {
-        updatePricesStep()
-
-        return new WorkflowResponse(void 0)
-    }
-)
-
-export default async function migrateNormalizeCurrencyCodes({ container }: ExecArgs) {
-    await normalizeCurrencyCodesWorkflow(container).run()
+  await knex.transaction(async (trx) => {
+    return await trx("price").update({
+      currency_code: knex.raw("LOWER(currency_code)"),
+    })
+  })
 }
