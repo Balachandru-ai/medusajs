5a84338f1118001bd3c2913692ed3132cbd72319
adrien2p <adrien.deperetti@gmail.com>
2025-12-02 13:58:31 +0100

    feat(): locale middleware

---
+++ packages/core/framework/src/http/middlewares/apply-locale.ts
@@ -0,0 +1,39 @@
+import { ContainerRegistrationKeys } from "@medusajs/utils"
+import type {
+  MedusaNextFunction,
+  MedusaRequest,
+  MedusaResponse,
+} from "../types"
+
+const CONTENT_LANGUAGE_HEADER = "content-language"
+
+/**
+ * Middleware that resolves the locale for the current request.
+ *
+ * Resolution order:
+ * 1. Query parameter `?locale=en-US`
+ * 2. Content-Language header
+ *
+ * The resolved locale is set on `req.locale`.
+ */
+export async function applyLocale(
+  req: MedusaRequest,
+  _: MedusaResponse,
+  next: MedusaNextFunction
+) {
+  // 1. Check query parameter
+  const queryLocale = req.query.locale as string | undefined
+  if (queryLocale) {
+    req.locale = queryLocale
+    return next()
+  }
+
+  // 2. Check Content-Language header
+  const headerLocale = req.get(CONTENT_LANGUAGE_HEADER)
+  if (headerLocale) {
+    req.locale = headerLocale
+    return next()
+  }
+
+  return next()
+}
