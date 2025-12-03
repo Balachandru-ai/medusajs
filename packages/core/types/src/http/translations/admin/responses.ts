6cc001b5e99ff650c77990aa435ea2e2574c5245
Nicolas Gorga <nicogorga11@gmail.com>
2025-12-02 23:53:35 -0300

    Types

--- packages/core/types/src/http/translations/admin/responses.ts
+++ packages/core/types/src/http/translations/admin/responses.ts
@@ -1,16 +1,35 @@
-import { TranslationDTO } from "../../../translation/common"
 import { PaginatedResponse } from "../../common"
+import { AdminTranslation } from "./entities"
 
 export interface AdminTranslationsResponse {
   /**
    * The list of translations.
    */
-  translation: TranslationDTO
+  translation: AdminTranslation
 }
 
 export type AdminTranslationsListResponse = PaginatedResponse<{
   /**
    * The list of translations.
    */
-  translations: TranslationDTO[]
+  translations: AdminTranslation[]
 }>
+
+export interface AdminTranslationsBatchResponse {
+  /**
+   * The created translations.
+   */
+  created: AdminTranslation[]
+  /**
+   * The updated translations.
+   */
+  updated: AdminTranslation[]
+  /**
+   * The deleted translations.
+   */
+  deleted: {
+    ids: string[]
+    object: "translation"
+    deleted: boolean
+  }
+}
