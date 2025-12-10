5f9b8f287f8637b2838f8ba98f5528019cc57642
Nicolas Gorga <nicogorga11@gmail.com>
2025-12-10 08:52:03 -0300

    Remove down method

--- packages/modules/order/src/migrations/Migration20251210112924.ts
+++ packages/modules/order/src/migrations/Migration20251210112924.ts
@@ -1,13 +1,9 @@
-import { Migration } from '@mikro-orm/migrations';
+import { Migration } from "@mikro-orm/migrations"
 
 export class Migration20251210112924 extends Migration {
-
   override async up(): Promise<void> {
-    this.addSql(`drop index if exists "IDX_unique_order_item_version_item_id";`);
+    this.addSql(`drop index if exists "IDX_unique_order_item_version_item_id";`)
   }
 
-  override async down(): Promise<void> {
-    this.addSql(`CREATE UNIQUE INDEX IF NOT EXISTS "IDX_unique_order_item_version_item_id" ON "order_item" ("version", "item_id") WHERE deleted_at IS NULL;`);
-  }
-
+  override async down(): Promise<void> {}
 }
