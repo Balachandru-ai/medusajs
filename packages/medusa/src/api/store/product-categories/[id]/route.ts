867dcfe694e629005f92ba0483cbfdf8e70cd01d
adrien2p <adrien.deperetti@gmail.com>
2025-12-15 16:53:56 +0100

    leave out shipping and return reason

--- packages/medusa/src/api/store/product-categories/[id]/route.ts
+++ packages/medusa/src/api/store/product-categories/[id]/route.ts
@@ -1,5 +1,5 @@
 import { StoreProductCategoryResponse } from "@medusajs/framework/types"
-import { MedusaError } from "@medusajs/framework/utils"
+import { applyTranslations, MedusaError } from "@medusajs/framework/utils"
 import {
   AuthenticatedMedusaRequest,
   MedusaResponse,
