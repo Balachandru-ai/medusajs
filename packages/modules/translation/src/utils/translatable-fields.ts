9177a1936942f63160bc36c5690f7674078f1afc
adrien2p <adrien.deperetti@gmail.com>
2025-12-15 16:29:15 +0100

    feat(): Add product type and collection translation support

--- packages/modules/translation/src/utils/translatable-fields.ts
+++ packages/modules/translation/src/utils/translatable-fields.ts
@@ -5,9 +5,17 @@ export const PRODUCT_TRANSLATABLE_FIELDS = [
   "subtitle",
   "status",
 ]
-
 export const PRODUCT_VARIANT_TRANSLATABLE_FIELDS = ["title", "material"]
-
 export const PRODUCT_TYPE_TRANSLATABLE_FIELDS = ["value"]
-
 export const PRODUCT_COLLECTION_TRANSLATABLE_FIELDS = ["title"]
+export const PRODUCT_CATEGORY_TRANSLATABLE_FIELDS = ["name", "description"]
+export const PRODUCT_TAG_TRANSLATABLE_FIELDS = ["value"]
+
+export const translatableFieldsConfig = {
+  product: PRODUCT_TRANSLATABLE_FIELDS,
+  product_variant: PRODUCT_VARIANT_TRANSLATABLE_FIELDS,
+  product_type: PRODUCT_TYPE_TRANSLATABLE_FIELDS,
+  product_collection: PRODUCT_COLLECTION_TRANSLATABLE_FIELDS,
+  product_category: PRODUCT_CATEGORY_TRANSLATABLE_FIELDS,
+  product_tag: PRODUCT_TAG_TRANSLATABLE_FIELDS,
+}
