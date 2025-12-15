d7288e7c2eaf78213b6b4bb35f8006755eb15267
adrien2p <adrien.deperetti@gmail.com>
2025-12-15 16:45:52 +0100

    fix

--- packages/modules/translation/src/loaders/config.ts
+++ packages/modules/translation/src/loaders/config.ts
@@ -1,13 +1,7 @@
 import { LoaderOptions } from "@medusajs/framework/types"
-import {
-  PRODUCT_COLLECTION_TRANSLATABLE_FIELDS,
-  PRODUCT_TRANSLATABLE_FIELDS,
-  PRODUCT_TYPE_TRANSLATABLE_FIELDS,
-  PRODUCT_VARIANT_TRANSLATABLE_FIELDS,
-  translatableFieldsConfig,
-} from "../utils/translatable-fields"
-import { asValue } from "awilix"
 import { TRANSLATABLE_FIELDS_CONFIG_KEY } from "@utils/constants"
+import { asValue } from "awilix"
+import { translatableFieldsConfig } from "../utils/translatable-fields"
 
 export default async ({ container }: LoaderOptions): Promise<void> => {
   container.register(
