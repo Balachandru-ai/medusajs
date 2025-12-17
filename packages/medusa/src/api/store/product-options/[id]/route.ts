c37ed4c409c37dfcaced039fa6a2597ecf360455
Shahed Nasser <shahednasser@gmail.com>
2025-12-17 11:22:05 +0200

    fix type parameters for store request

--- packages/medusa/src/api/store/product-options/[id]/route.ts
+++ packages/medusa/src/api/store/product-options/[id]/route.ts
@@ -9,7 +9,7 @@ import {
 } from "@medusajs/framework/http"
 
 export const GET = async (
-  req: AuthenticatedMedusaRequest<HttpTypes.SelectParams>,
+  req: AuthenticatedMedusaRequest<{}, HttpTypes.SelectParams>,
   res: MedusaResponse<HttpTypes.StoreProductOptionResponse>
 ) => {
   const query = req.scope.resolve(ContainerRegistrationKeys.QUERY)
