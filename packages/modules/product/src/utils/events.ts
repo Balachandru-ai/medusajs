aae20ef2dbd5b83de74c89ac22abe0cf17389dfa
adrien2p <adrien.deperetti@gmail.com>
2026-01-07 15:18:28 +0100

    wip

--- packages/modules/product/src/utils/events.ts
+++ packages/modules/product/src/utils/events.ts
@@ -2,6 +2,7 @@ import {
   CommonEvents,
   moduleEventBuilderFactory,
   Modules,
+  ProductEvents,
 } from "@medusajs/framework/utils"
 
 export const eventBuilders = {
@@ -9,15 +10,18 @@ export const eventBuilders = {
     source: Modules.PRODUCT,
     action: CommonEvents.CREATED,
     object: "product_category",
+    eventName: ProductEvents.PRODUCT_CATEGORY_CREATED,
   }),
   updatedProductCategory: moduleEventBuilderFactory({
     source: Modules.PRODUCT,
     action: CommonEvents.UPDATED,
     object: "product_category",
+    eventName: ProductEvents.PRODUCT_CATEGORY_UPDATED,
   }),
   deletedProductCategory: moduleEventBuilderFactory({
     source: Modules.PRODUCT,
     action: CommonEvents.DELETED,
     object: "product_category",
+    eventName: ProductEvents.PRODUCT_CATEGORY_DELETED,
   }),
 }
