a73b8e0b91b81c41441ed4c266e6cef43f664965
fPolic <mainacc.polic@gmail.com>
2026-01-09 16:52:00 +0100

    refactor: use query

--- packages/medusa/src/api/admin/products/[id]/options/batch/route.ts
+++ packages/medusa/src/api/admin/products/[id]/options/batch/route.ts
@@ -1,10 +1,10 @@
 import {
   AuthenticatedMedusaRequest,
   MedusaResponse,
-  refetchEntity,
 } from "@medusajs/framework/http"
 import { HttpTypes } from "@medusajs/framework/types"
 import { createAndLinkProductOptionsToProductWorkflow } from "@medusajs/core-flows"
+import { ContainerRegistrationKeys } from "@medusajs/framework/utils"
 import { remapKeysForProduct, remapProductResponse } from "../../../helpers"
 
 export const POST = async (
@@ -13,6 +13,8 @@ export const POST = async (
 ) => {
   const productId = req.params.id
 
+  const query = req.scope.resolve(ContainerRegistrationKeys.QUERY)
+
   await createAndLinkProductOptionsToProductWorkflow(req.scope).run({
     input: {
       product_id: productId,
@@ -20,12 +22,12 @@ export const POST = async (
     },
   })
 
-  const product = await refetchEntity({
+  const { data: products } = await query.graph({
     entity: "product",
-    idOrFilter: productId,
-    scope: req.scope,
+    filters: { id: productId },
     fields: remapKeysForProduct(req.queryConfig.fields ?? []),
   })
+  const product = products[0]
 
   res.status(200).json({ product: remapProductResponse(product) })
 }
