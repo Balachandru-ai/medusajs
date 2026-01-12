1155381e194ff0a96d6b0233adcd4ca64e2c9c32
Nicolas Gorga <nicogorga11@gmail.com>
2026-01-12 16:31:34 -0300

    Remove new indes on order line item adjustment and version

--- packages/modules/order/src/migrations/Migration20260106185528.ts
+++ packages/modules/order/src/migrations/Migration20260106185528.ts
@@ -3,10 +3,6 @@ import { Migration } from "@medusajs/framework/mikro-orm/migrations"
 export class Migration20260106185528 extends Migration {
   override async up(): Promise<void> {
     this.addSql(
-      `CREATE UNIQUE INDEX IF NOT EXISTS "IDX_order_line_item_adjustment_version_item" ON "order_line_item_adjustment" ("version", "item_id") WHERE deleted_at IS NULL;`
-    )
-
-    this.addSql(
       `alter table if exists "order_shipping_method_adjustment" add column if not exists "version" integer not null default 1;`
     )
     this.addSql(
@@ -29,10 +25,6 @@ export class Migration20260106185528 extends Migration {
   }
 
   override async down(): Promise<void> {
-    this.addSql(
-      `drop index if exists "IDX_order_line_item_adjustment_version_item";`
-    )
-
     this.addSql(
       `drop index if exists "IDX_order_shipping_method_adjustment_version_shipping_method";`
     )
