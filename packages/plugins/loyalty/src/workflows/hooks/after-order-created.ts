b277fad83dc433b3ada39cdbcc08bafc46bc4724
Nicolas Gorga <nicogorga11@gmail.com>
2026-02-09 22:29:22 -0300

    Avoid releasing events in hooks workflow executions

--- packages/plugins/loyalty/src/workflows/hooks/after-order-created.ts
+++ packages/plugins/loyalty/src/workflows/hooks/after-order-created.ts
@@ -1,12 +1,11 @@
-import { StepResponse } from "@medusajs/framework/workflows-sdk";
-import { completeCartWorkflow } from "@medusajs/medusa/core-flows";
-import { confirmCartCreditLinesWorkflow } from "../carts/workflows/confirm-cart-credit-lines";
-import { cloneCartGiftCardsToOrderWorkflow } from "../orders/workflows/link-gift-cards-to-order";
-
-(completeCartWorkflow.hooks as any).orderCreated(
+import { StepResponse } from "@medusajs/framework/workflows-sdk"
+import { completeCartWorkflow } from "@medusajs/medusa/core-flows"
+import { confirmCartCreditLinesWorkflow } from "../carts/workflows/confirm-cart-credit-lines"
+import { cloneCartGiftCardsToOrderWorkflow } from "../orders/workflows/link-gift-cards-to-order"
+;(completeCartWorkflow.hooks as any).orderCreated(
   async (data: { order_id: string; cart_id: string }, stepContext) => {
-    const { container, ...sharedContext } = stepContext;
-    const { order_id, cart_id } = data;
+    const { container, ...sharedContext } = stepContext
+    const { order_id, cart_id } = data
 
     const transaction = await cloneCartGiftCardsToOrderWorkflow.run({
       input: {
@@ -14,24 +13,24 @@ import { cloneCartGiftCardsToOrderWorkflow } from "../orders/workflows/link-gift
         cart_id: cart_id,
       },
       container,
-      context: { ...sharedContext },
-    });
+      context: { ...sharedContext, preventReleaseEvents: true },
+    })
 
-    const { result } = transaction;
+    const { result } = transaction
 
-    return new StepResponse(result, stepContext.transactionId);
+    return new StepResponse(result, stepContext.transactionId)
   },
   async (transactionId, stepContext) => {
     if (!transactionId) {
-      return;
+      return
     }
 
-    const { container, ...sharedContext } = stepContext;
+    const { container, ...sharedContext } = stepContext
 
     await confirmCartCreditLinesWorkflow(container).cancel({
       transactionId,
       container,
       context: { ...sharedContext },
-    });
+    })
   }
-);
+)
